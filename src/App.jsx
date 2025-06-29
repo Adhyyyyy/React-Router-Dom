986f5725fc9080d5ca25adc8bf964cb96f8ba12c
Temporary User <temp@example.com>
2025-06-29 09:19:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FetchDemo from "./FetchDemo";
+import "./App.css";
+function App(){
+  return(
+    <div>
+      <h1>Custom useFetch Hook Demo</h1>
+      <FetchDemo/>
+    </div>
+  )
+}
+
+export default App;
